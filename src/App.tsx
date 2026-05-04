cdb3a8d20a9971805b93e8b0148c7de8b62709c9
Krishna Pradhan <122208388+Krishney21@users.noreply.github.com>
2026-05-04 19:49:03 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -113,7 +113,7 @@ function App() {
               ))}
               <button
                 className="bg-blue-600 text-white px-4 py-2 rounded-md text-sm font-medium flex items-center"
-                onClick={() => window.open('https://drive.google.com/file/d/1ao-fbPp-kx5Vmsv8KFM1tZSIvd3E7067/view?usp=sharing')}
+                onClick={() => window.open('https://drive.google.com/file/d/1MfQuGoa6Jb-aRd4Srewxrd0XVxRzcoJi/view?usp=sharing')}
               >
                 <Download className="w-4 h-4 mr-2" />
                 Download CV
